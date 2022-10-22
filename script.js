62748dbd159a9512b484f16e4842436981d8ff27
jnr <jrveneracion13@gmail.com>
2022-10-22 19:03:56 +0800

    contact position fixed

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function displayProfile(){
 function displayContact(){
     profile.style = "animation: close 1s ease; animation-fill-mode: forwards;";
     contact.style = "display: block; animation: open 1s ease; animation-fill-mode: forwards;";
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    // window.scrollTo({ top: 0, behavior: 'smooth' });
 
     setTimeout(function() {
         profile.style.display = "none";
