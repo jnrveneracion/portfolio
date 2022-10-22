156fa9632a40b88e5bcaed113d16439bb47877d8
jnri <91825003+jnrveneracion@users.noreply.github.com>
2022-10-22 19:12:38 +0800

    merge

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ setTimeout(function() {
 
 
 function displayProfile(){
-    profile.style = "animation: open 1s ease; animation-fill-mode: forwards;";
+    profile.style = "display: block; animation: open 1s ease; animation-fill-mode: forwards;";
     contact.style = "animation: close 1s ease; animation-fill-mode: forwards;";
 
     setTimeout(function() {
@@ -29,7 +29,7 @@ function displayProfile(){
 }
 
 function displayContact(){
-    profile.style = "display: block; animation: close 1s ease; animation-fill-mode: forwards;";
+    profile.style = "animation: close 1s ease; animation-fill-mode: forwards;";
     contact.style = "display: block; animation: open 1s ease; animation-fill-mode: forwards;";
     // window.scrollTo({ top: 0, behavior: 'smooth' });
 
