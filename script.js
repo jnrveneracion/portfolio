d6cc3696c3c679f68a6aa0207fcae5c809bd1172
jnr <jrveneracion13@gmail.com>
2022-10-23 11:37:10 +0800

    script display to sticky

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ setTimeout(function() {
 
 
 function displayProfile(){
-    profile.style = "display: block; animation: open 1s ease; animation-fill-mode: forwards;";
+    profile.style = "display: sticky; animation: open 1s ease; animation-fill-mode: forwards;";
     contact.style = "animation: close 1s ease; animation-fill-mode: forwards;";
     window.scrollTo({ top: 0, behavior: 'smooth' });
 
@@ -31,7 +31,7 @@ function displayProfile(){
 
 function displayContact(){
     profile.style = "animation: close 1s ease; animation-fill-mode: forwards;";
-    contact.style = "display: block; animation: open 1s ease; animation-fill-mode: forwards;";
+    contact.style = "display: sticky; animation: open 1s ease; animation-fill-mode: forwards;";
     window.scrollTo({ top: 0, behavior: 'smooth' });
 
     setTimeout(function() {
